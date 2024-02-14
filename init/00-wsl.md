e73b61b7244fea1092382f8e7276c9c07baa04c4
mogg <m1664907189@gmail.com>
2024-02-14 17:28:34 +0800

    feat: add cmake

--- init/00-wsl.md
+++ init/00-wsl.md
@@ -5,8 +5,19 @@ https://learn.microsoft.com/zh-cn/windows/wsl/install
 
 https://github.com/sorin-ionescu/prezto/issues/1820#issuecomment-690138955
 
+wsl `sudo vim /etc/wsl.conf`
+
 ```
 [interop]
 enabled=true # WSL 是否支持启动 Windows 进程。 for code
 appendWindowsPath=false # WSL 是否将 Windows 路径元素添加到$PATH环境变量。
+```
+
+## auto net proxy
+
+win `code %USERPROFILE%\.wslconfig`
+
+```
+[experimental]
+autoProxy=false
 ```
--- init/02-cpp.sh
+++ init/02-cpp.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+cd /tmp
+
 # xmake https://xmake.io/#/zh-cn/getting_started
 curl -fsSL https://xmake.io/shget.text | bash
 
@@ -18,3 +20,19 @@ LLVM_VERSION=$(/tmp/llvm.sh -h 2>&1 | grep -oP 'CURRENT_LLVM_STABLE=\K[^ ]+')
 SHELL_FOLDER=$(cd "$(dirname "$0")"; cd ../ ; pwd)
 sudo $SHELL_FOLDER/bin/update-alternatives-clang.sh $LLVM_VERSION 100
 
+# cmake
+wget https://github.com/Kitware/CMake/releases/latest
+# 使用wget获取最新的发布版本信息 && 提取版本号（假设版本号格式为vX.Y.Z）
+version=$(wget -qO- https://api.github.com/repos/Kitware/CMake/releases/latest | grep 'tag_name' | cut -d\" -f4 | sed 's/v//')
+# 拼接下载链接
+cmake_name="cmake-$version"
+download_url="https://github.com/Kitware/CMake/releases/download/v$version/$cmake_name.tar.gz"
+echo $cmake_name
+# 使用wget下载文件
+wget -O /tmp/$cmake_name.tar.gz $download_url
+tar zxvf $cmake_name.tar.gz
+cd $cmake_name
+sudo ./bootstrap
+sudo make -j 24
+sudo make install
+cmake  --version 
